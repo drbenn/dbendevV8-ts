6a2b7febd6cc9be12bfa181bc41b883280c0fd62
drbenn <bennett.daniel@gmail.com>
2025-06-06 21:50:35 -0400

    tailwind and daisyui installed and configured, three and three-ts installed

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,22 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
+    <div class="mb-5">
+        <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
+    </div>
+    <button class="btn" onclick="my_modal_1.showModal()">open modal</button>
+    <dialog id="my_modal_1" class="modal">
+      <div class="modal-box">
+        <h3 class="text-lg font-bold">Hello!</h3>
+        <p class="py-4">Press ESC key or click the button below to close</p>
+        <div class="modal-action">
+          <form method="dialog">
+            <!-- if there is a button in form, it will close the modal -->
+            <button class="btn">Close</button>
+          </form>
+        </div>
+      </div>
+    </dialog>
   </div>
 `
 
